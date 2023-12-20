7c82bde7bba2b640393573482fa2bc4bb4b43fd1
Jaret Hunemiller <jchunemiller@gmail.com>
2023-12-20 09:49:20 -0700

    mobile

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,32 +2,31 @@
   <div class="container-fluid p-5 background align-items-center">
 
 
-    <section class="row  p-5 align-items-center">
+    <section class="row justify-content-center  p-5 align-items-center">
 
-      <div class="d-flex justify-content-center">
 
-        <div class="col-5 text-center text-light p-5">
-          <div class="p-5">
-            <p class="fs-1 topText text-light fw-bold">Jaret Hunemiller</p>
-          </div>
-          <p class="fs-1 topText">Software Developer / Engineer</p>
-          <p class="fs-3 topText">"Programming is not about typing, it's about thinking." - Rich Hickey</p>
+      <div class="col-md-5 col-12">
+        <img class="rounded box img-fluid"
+          src="https://media.licdn.com/dms/image/D4D03AQEyrBuVdW2g0w/profile-displayphoto-shrink_400_400/0/1701385590406?e=1708560000&v=beta&t=BqEfgUnJgR4Gfm7eZxW8pIKB37JVJjyHI0g6ZbtDhOE"
+          alt="">
+      </div>
 
-          <div class="d-flex justify-content-center">
-            <p role="button"><a href="https://www.linkedin.com/in/jaret-hunemiller-000491296/"><i
-                  class="mdi mdi-linkedin fs-1 text-light"></i></a></p>
-            <p role="button"><a href="https://github.com/JCHU14"><i class="mdi mdi-github fs-1 text-light"></i></a></p>
-          </div>
-
+      <div class="col-md-5 col-12 text-center text-light">
+        <div class="">
+          <p class="fs-1 topText text-light fw-bold">Jaret Hunemiller</p>
         </div>
+        <p class="fs-1 topText">Software Developer / Engineer</p>
+        <p class="fs-3 topText">"Programming is not about typing, it's about thinking." - Rich Hickey</p>
 
-        <div class="col-5 p-5">
-          <img class="rounded box"
-            src="https://media.licdn.com/dms/image/D4D03AQEyrBuVdW2g0w/profile-displayphoto-shrink_400_400/0/1701385590406?e=1708560000&v=beta&t=BqEfgUnJgR4Gfm7eZxW8pIKB37JVJjyHI0g6ZbtDhOE"
-            alt="">
+        <div class="d-flex justify-content-center">
+          <p role="button"><a href="https://www.linkedin.com/in/jaret-hunemiller-000491296/"><i
+                class="mdi mdi-linkedin fs-1 text-light"></i></a></p>
+          <p role="button"><a href="https://github.com/JCHU14"><i class="mdi mdi-github fs-1 text-light"></i></a></p>
         </div>
 
       </div>
+
+
 
     </section>
 
@@ -39,9 +38,9 @@
 
     <section class="row">
 
-      <div class="d-flex justify-content-center">
+      <div class="  d-sm-flex justify-content-center">
 
-        <div class="col-5 p-5">
+        <div class="col-md-5 col-12 py-5">
           <p class="fs-1 text-light text-center fw-bold">About Me</p>
           <p class="text-light text-center fs-5">Hello I am Jaret Hunemiller, I am a full stack developer/engineer in the
             Boise
@@ -53,8 +52,8 @@
           </p>
         </div>
 
-        <div class="col-5 p-5">
-          <img class="smallImg box"
+        <div class="col-md-5 col-12 p-5">
+          <img class="smallImg box img-fluid"
             src="https://th.bing.com/th?id=OIP.70RXkdZQphbqava-x_g9BQHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
             alt="">
         </div>
@@ -71,7 +70,7 @@
 
 
 
-      <div class="col-4 text-center p-5">
+      <div class="col-md-4 col-12 text-center p-5">
         <p class="fs-3 fw-bold underline-2">Libraries and Tools</p>
         <div class="text-left underline fs-4">
 
@@ -87,7 +86,7 @@
         </div>
       </div>
 
-      <div class="col-4 text-center p-5">
+      <div class="col-md-4 col-12 text-center p-5">
         <p class="fs-3  fw-bold underline-2">Languages</p>
         <div class="underline fs-4">
           <p>JavaScript(client/server)</p>
@@ -98,7 +97,7 @@
         </div>
       </div>
 
-      <div class="col-4 text-center p-5">
+      <div class="col-md-4 col-12 text-center p-5">
         <p class="fw-bold  fs-3 underline-2">Frameworks</p>
 
         <div class="underline fs-4">
@@ -118,37 +117,26 @@
 
   <div class="container-fluid p-5 bg-dark">
 
-    <section class="row">
-      <p class="fs-1 fw-bold text-light p-5">My Projects</p>
-      <div class="d-flex justify-content-center">
+    <section class="row justify-content-center">
+      <p class="fs-1 fw-bold text-light text-center p-5">My Projects</p>
 
-        <div class="col-5 text-center">
-          <p class="fs-1 fw-bold text-light p-5"><em><i class="mdi mdi-tire logo mx-1 mdi-spin"></i>Auto-Tunes</em></p>
-          <img class="project-photo text-end img-fluid rounded-circle"
-            src="https://th.bing.com/th/id/R.06bbd1f7e6ad1f4291d9d6e9796d30c4?rik=tmM8PTZXPoGW3w&pid=ImgRaw&r=0" alt="">
-          <p class="fs-5 text-light p-5 underline">An car scene community page for all to enjoy!</p>
-        </div>
 
-        <div class="col-5 text-center">
-          <p class="fs-1 fw-bold text-light p-5">Tower</p>
-          <img class="project-photo" src="https://justincarpenter2020.github.io/assets/images/towerLogo.svg" alt="">
-          <p class="fs-5 p-5 text-light underline">Do you like events? Well tower is the perfect place for you to get your
-            tickets!</p>
-        </div>
-
+      <div class="col-md-5 col-12 text-center">
+        <p class="fs-1 fw-bold text-light p-5"><em><i class="mdi mdi-tire logo mx-1 mdi-spin"></i>Auto-Tunes</em></p>
+        <img class="project-photo img-fluid text-end img-fluid rounded-circle"
+          src="https://th.bing.com/th/id/R.06bbd1f7e6ad1f4291d9d6e9796d30c4?rik=tmM8PTZXPoGW3w&pid=ImgRaw&r=0" alt="">
+        <p class="fs-5 text-light p-5 underline">An car scene community page for all to enjoy!</p>
       </div>
 
-      <div class="d-flex justify-content-center">
-
-        <div class="col-5 p-5">
-          <p class="text-light fs-3">Coming Soon</p>
-        </div>
-
-        <div class="col-5 p-5">
-          <p class="text-light fs-3">Coming Soon</p>
-        </div>
-
+      <div class="col-md-5 col-12 text-center">
+        <p class="fs-1 fw-bold text-light p-5">Tower</p>
+        <img class="project-photo img-fluid" src="https://justincarpenter2020.github.io/assets/images/towerLogo.svg"
+          alt="">
+        <p class="fs-5 p-5 text-light underline">Do you like events? Well tower is the perfect place for you to get your
+          tickets!</p>
       </div>
+
+
 
     </section>
 
